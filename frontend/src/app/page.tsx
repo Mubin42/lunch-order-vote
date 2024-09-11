96ff8991a7a39bfb312e531bf050d70602eeb9a7
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2024-09-12 01:18:46 +0600

    frontend components added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,35 @@
+'use client';
 import FoodCard from '@/components/food-card/FoodCard';
 import Navbar from '@/components/navbar/Navbar';
+import { getDay } from '@/functions/getDay';
 import { BASE_PADDING } from '@/lib/constants';
-import { Flex, Heading, Select, Stack } from '@chakra-ui/react';
+import { Flex, Heading, Input, Select, Stack } from '@chakra-ui/react';
+import { useState } from 'react';
 
 export default function Home() {
+	const [date, setDate] = useState('');
+
+	const day = getDay(date);
+
 	return (
 		<Flex w='100vw' h='100vh' justify='center'>
-			<Flex w='1280px' bg='whitesmoke' flexDirection='column' gap={8}>
+			<Flex w='1280px' bg='whitesmoke' flexDirection='column' gap={8} overflowY='scroll'>
 				<Navbar />
-
 				<Stack px={BASE_PADDING}>
 					<Flex py={4} justify='space-between'>
-						<Heading size='md'>Popular Foods</Heading>
-						<Select placeholder='today'>
-							<option value='option1'>Option 1</option>
-							<option value='option2'>Option 2</option>
-							<option value='option3'>Option 3</option>
-						</Select>
+						<Heading size='md'>Menu {date && day}</Heading>
+						<Flex gap={2} align='center'>
+							Choose Date:
+							<Input
+								w={48}
+								bg='white'
+								type='date'
+								value={date}
+								onChange={(e) => setDate(e.target.value)}
+							/>
+						</Flex>
 					</Flex>
+					<FoodCard />
 					<FoodCard />
 					<FoodCard />
 					<FoodCard />
